a5ad4070ec596852c3cd6667ed70b94b7fc7216f
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-11-07 20:22:15 +0200

    lesson-8

---
+++ lesson-8/LUtil.cpp
@@ -0,0 +1,70 @@
+#include "LUtil.h"
+#include <IL/il.h>
+#include <IL/ilu.h>
+#include "LTexture.h"
+
+LTexture gNon2NTexture;
+
+bool initGL()
+{
+  glViewport( 0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT );
+
+  glMatrixMode( GL_PROJECTION );
+  glLoadIdentity();
+  glOrtho( 0.0, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0, 1.0, -1.0 );
+
+  glMatrixMode( GL_MODELVIEW );
+  glLoadIdentity();
+
+  glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+
+  glEnable( GL_TEXTURE_2D );
+
+  GLenum error = glGetError();
+  if( error != GL_NO_ERROR )
+  {
+    printf( "Error initilizing OpenGL! %s\n", gluErrorString( error ) );
+    return false;
+  }
+
+  ilInit();
+  iluInit();
+  ilClearColour( 255, 255, 255, 000 );
+
+  ILenum ilError = ilGetError();
+  if( ilError != IL_NO_ERROR )
+  {
+    printf( "Error initilizaing DevIL! %s\n", iluErrorString( ilError ) );
+    return false;
+  }
+
+  return true;
+}
+
+bool loadMedia()
+{
+  if ( !gNon2NTexture.loadTexttureFromFile( "./opengl.png" ) )
+  {
+    printf( "Unable to load non-power-of-two texture!\n" );
+    return false;
+  }
+
+  return true;
+}
+
+void update()
+{
+
+}
+
+void render()
+{
+  glClear( GL_COLOR_BUFFER_BIT );
+
+  gNon2NTexture.render(
+    ( SCREEN_WIDTH - gNon2NTexture.imageWidth() ) / 2.f,
+    ( SCREEN_HEIGHT - gNon2NTexture.imageHeight() ) / 2.f
+  );
+
+  glutSwapBuffers();
+}
---
+++ lesson-8/main.cpp
@@ -0,0 +1,42 @@
+#include "LUtil.h"
+
+void runMainLoop( int val );
+
+int main( int argc, char* args[] )
+{
+  glutInit( &argc, args );
+
+  glutInitContextVersion( 2, 1 );
+
+  glutInitDisplayMode( GLUT_DOUBLE );
+  glutInitWindowSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+  glutCreateWindow( "OpenGL" );
+
+  if ( !initGL() )
+  {
+    printf( "Unable to initialize graphics library!" );
+    return 1;
+  }
+
+  if( !loadMedia() )
+  {
+    printf( "Unable to load media!\n" );
+    return 2;
+  }
+
+  glutDisplayFunc( render );
+
+  glutTimerFunc( 1000 / SCREEN_FPS, runMainLoop, 0 );
+
+  glutMainLoop();
+
+  return 0;
+}
+
+void runMainLoop( int val )
+{
+  update();
+  render();
+
+  glutTimerFunc( 1000 / SCREEN_FPS, runMainLoop, val );
+}
