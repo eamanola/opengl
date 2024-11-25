3ecb991299bbb6c087d529cf45ad72523c794908
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-11-25 16:55:13 +0200

    lesson-12

---
+++ lesson-12/LTexture.h
@@ -0,0 +1,65 @@
+#ifndef LTEXTURE_H
+#define LTEXTURE_H
+
+#include "../LOpenGL.h"
+#include "LFRect.h"
+#include <string>
+
+class LTexture
+{
+  public:
+    LTexture();
+    ~LTexture();
+
+    void freeTexture();
+
+    void render(
+      GLfloat x, GLfloat y, LFRect* clip = NULL, LFRect* stretch = NULL, GLfloat degrees = 0.f
+    );
+
+    GLuint getTextureID();
+
+    GLuint textureWidth();
+    GLuint textureHeight();
+
+    GLuint imageWidth();
+    GLuint imageHeight();
+
+    bool lock();
+    bool unlock();
+
+    GLuint* getPixelData32();
+    GLuint getPixel32( GLuint x, GLuint y );
+    void setPixel32( GLuint x, GLuint y, GLuint pixel );
+
+    bool loadTextureFromFileWithColorKey(
+      std::string path, GLubyte r, GLubyte g, GLubyte b, GLubyte a = 000
+    );
+
+    bool loadTextureFromFile( std::string );
+
+  protected:
+    bool loadPixelsFromFile( std::string );
+    bool loadTextureFromPixels32();
+
+    bool loadTextureFromPixels32(
+      GLuint* pixels,
+      GLuint imgWidth, GLuint imgHeight,
+      GLuint texWidth, GLuint texHeight
+    );
+
+  private:
+    GLuint powerOfTwo( GLuint num );
+
+    GLuint mTextureID;
+
+    GLuint* mPixels;
+
+    GLuint mTextureWidth;
+    GLuint mTextureHeight;
+
+    GLuint mImageWidth;
+    GLuint mImageHeight;
+};
+
+#endif
---
+++ lesson-12/LUtil.cpp
@@ -0,0 +1,85 @@
+#include "LUtil.h"
+#include <IL/il.h>
+#include <IL/ilu.h>
+#include "LTexture.h"
+
+LTexture gRotatingTexture;
+GLfloat gAngle = 0.f;
+
+bool initGL()
+{
+  glViewport( 0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT );
+
+  glMatrixMode( GL_PROJECTION );
+  glLoadIdentity();
+  glOrtho( 0.0, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0, 1.0, -1.0 );
+
+  glMatrixMode( GL_MODELVIEW );
+  glLoadIdentity();
+
+  glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+
+  glEnable( GL_TEXTURE_2D );
+
+  glEnable( GL_BLEND );
+  glDisable( GL_DEPTH_TEST );
+  glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+
+  GLenum error = glGetError();
+  if( error != GL_NO_ERROR )
+  {
+    printf( "Error initilizing OpenGL! %s\n", gluErrorString( error ) );
+    return false;
+  }
+
+  ilInit();
+  iluInit();
+  ilClearColour( 255, 255, 255, 000 );
+
+  ILenum ilError = ilGetError();
+  if( ilError != IL_NO_ERROR )
+  {
+    printf( "Error initilizaing DevIL! %s\n", iluErrorString( ilError ) );
+    return false;
+  }
+
+  return true;
+}
+
+bool loadMedia()
+{
+  if ( !gRotatingTexture.loadTextureFromFile( "./arrow.png" ) )
+  {
+    printf( "Unable to load arrow texture!\n" );
+    return false;
+  }
+
+  return true;
+}
+
+void update()
+{
+  gAngle += 360.f / SCREEN_FPS;
+
+  if( gAngle > 360.f )
+  {
+    gAngle -= 360.f;
+  }
+}
+
+void render()
+{
+  glClear( GL_COLOR_BUFFER_BIT );
+
+  gRotatingTexture.render(
+    ( ( SCREEN_WIDTH - gRotatingTexture.imageWidth() ) / 2.f ),
+    ( SCREEN_HEIGHT - gRotatingTexture.imageHeight() ) / 2.f,
+    NULL, NULL, gAngle
+  );
+
+  glutSwapBuffers();
+}
+
+void handleKeys( unsigned char key, int x, int y )
+{
+}
